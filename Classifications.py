7f315ebcbf74119f565993bba73c5ddd6b793ed6
Mehrdad Naderi <43063581+mehnaderi@users.noreply.github.com>
2020-07-21 18:42:18 +0430

    Update Classifications.py

--- Classifications.py
+++ Classifications.py
@@ -4,9 +4,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
 # Importing the dataset
-dataset = pd.read_csv('twitterDS.csv')
+dataset = pd.read_csv('finalFile_v11.csv')
 X = dataset.iloc[:, [2, 6]].values
 y = dataset.iloc[:, 7].values
 
@@ -20,12 +21,13 @@ classifier1 = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
 classifier1.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred1 = classifier.predict(X_test)
+y_pred1 = classifier1.predict(X_test)
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cmForKNN = confusion_matrix(y_test, y_pred1)
-
+recallKNN = recall_score(y_test, y_pred1, average='macro')
+precisionKNN = precision_score(y_test, y_pred1, average='macro')
 
 # Fitting Decision Tree Classification to the Training set
 from sklearn.tree import DecisionTreeClassifier
@@ -33,10 +35,11 @@ classifier2 = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier2.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred2 = classifier.predict(X_test)
+y_pred2 = classifier2.predict(X_test)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+recallDT = recall_score(y_test, y_pred2, average='macro')
+precisionDT = precision_score(y_test, y_pred2, average='macro')
+
 cmForDT = confusion_matrix(y_test, y_pred2)
 
 
@@ -46,8 +49,9 @@ classifier3 = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', r
 classifier3.fit(X_train, y_train)
 
 # Predicting the Test set results
-y_pred3 = classifier.predict(X_test)
+y_pred3 = classifier3.predict(X_test)
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
+recallRF = recall_score(y_test, y_pred3, average='macro')
+precisionRF = precision_score(y_test, y_pred3, average='macro')
+
 cmForRF = confusion_matrix(y_test, y_pred3)
